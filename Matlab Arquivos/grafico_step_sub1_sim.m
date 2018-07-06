0f758483ace31b5653fae9566de24d5bdc763805
TheNumbers <mateus.lucena.work@gmail.com>
2018-07-06 18:40:16 -0400

    r

---
+++ Matlab Arquivos/grafico_step_sub1_sim.m
@@ -0,0 +1,37 @@
+if max(y)<20
+    p1=0.1;
+    p2=-3;
+    y2=y;
+    y=(y-p2)/p1;
+    ye=(ye-p2)/p1;
+    ref2=ref;
+    ref=(ref-p2)/p1;
+end
+close all
+figure('position', [50, 50, 1200, 500],'Color',[1 1 1])
+plot(y,'k','LineWidth',1.5)
+hold on
+plot(ye,'LineWidth',1.5)
+plot(ref,'r','LineWidth',1.5)
+H=legend('$y_{medido}$','$y_{estimado}$', '$r$', 'Location', 'southeast', 'Orientation', 'vertical');
+set(H, 'Interpreter', 'latex', 'Fontsize', 22);
+ylabel('Altura (cm)', 'fontsize', 22)
+xlabel('Tempo (s)', 'fontsize', 22);
+T=title('Resposta ao degrau simulada do modelo SUB1');
+set(gca, 'fontsize', 20, 'Position',[0.155    0.16    0.8    0.75]);
+axis([0 10 min(min(y))-.1 max(max(y))+.1]);
+
+figure('position', [50, 50, 1200, 500],'Color',[1 1 1])
+plot(y2-ref2,'k','LineWidth',1.5)
+hold on
+plot(u,'LineWidth',1.5)
+H=legend('$e$','$u$', '$r$', 'Location', 'southeast', 'Orientation', 'vertical');
+set(H, 'Interpreter', 'latex', 'Fontsize', 22);
+% ylabel('Altura (cm)', 'fontsize', 22)
+xlabel('Tempo (s)', 'fontsize', 22);
+T=title('Resposta ao degrau simulada do modelo SUB1');
+set(gca, 'fontsize', 20, 'Position',[0.155    0.16    0.8    0.75]);
+axis([0 10 min(min(y2-ref2))-.1 max(max(u))+.1]);
+
+
+
