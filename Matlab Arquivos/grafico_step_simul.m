7160056bbc57fa5632c4801e10abc913788d69a0
TheNumbers <mateus.lucena.work@gmail.com>
2018-07-11 12:59:24 -0400

    r

--- Matlab Arquivos/grafico_step_simul.m
+++ Matlab Arquivos/grafico_step_simul.m
@@ -19,9 +19,9 @@ H=legend('$y_e$','$r$', '$y$', 'Location', 'northwest', 'Orientation', 'vertical
 set(H, 'Interpreter', 'latex', 'Fontsize', 22);
 ylabel('Altura (cm)', 'fontsize', 22)
 xlabel('Tempo (s)', 'fontsize', 22);
-T=title('Resposta à escadaria do modelo ARXsim simulado');
+T=title('Resposta à escadaria do modelo ARX1 simulado');
 set(gca, 'fontsize', 20, 'Position',[0.155    0.16    0.8    0.75]);
-axis([0 40 min(min(ye))-.1 max(max(y))+.1]);
+axis([0 40 min(min(ye))-.1 max(max(ye))+.1]);
 
 figure('position', [50, 50, 1200, 500],'Color',[1 1 1])
 stairs(t,y2-ref2,'k','LineWidth',1.5)
@@ -31,7 +31,7 @@ H=legend('$e$','$u$', '$r$', 'Location', 'southeast', 'Orientation', 'vertical')
 set(H, 'Interpreter', 'latex', 'Fontsize', 22);
 % ylabel('Altura (cm)', 'fontsize', 22)
 xlabel('Tempo (s)', 'fontsize', 22);
-T=title('Resposta à escadaria do modelo ARXsim simulado');
+T=title('Resposta à escadaria do modelo ARX1 simulado');
 set(gca, 'fontsize', 20, 'Position',[0.155    0.16    0.8    0.75]);
 axis([0 40 min(min(y2-ref2))-.1 max(max(u))+.1]);
 
