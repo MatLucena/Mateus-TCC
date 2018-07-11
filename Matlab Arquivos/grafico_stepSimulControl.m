51477da719e99d75d7fa68fe3515a93e88e636fa
TheNumbers <mateus.lucena.work@gmail.com>
2018-07-11 01:23:12 -0400

    r

--- Matlab Arquivos/grafico_stepSimulControl.m
+++ Matlab Arquivos/grafico_stepSimulControl.m
@@ -1,22 +1,29 @@
+if max(y)<20
+    p1=0.1;
+    p2=-3;
+    y2=y;
+    y=(y-p2)/p1;
+    ye=ye;
+    ye=(ye-p2)/p1;
+    ref2=ref;
+    ref=(ref-p2)/p1;
+end
 close all
 % G=ss(A1,B1,C1,D1,0.05);
-t=0:0.05:(1400)*0.05;
+t=0:0.05:(length(y)-1)*0.05;
 figure('position', [50, 50, 1200, 500],'Color',[1 1 1])
-u_t=ones(1,length(t));
-y=simout.Data';
-yr=(y-p2)/p1;
 % u_t1=ones(1,length(yr))*yr(end);
-stairs(simout.Time,yr(1,:),':','LineWidth',2)
+stairs(t,ref,':','LineWidth',2)
 hold on
-stairs(simout.Time,yr(2,:),'k','LineWidth',2)
-stairs(simout.Time,yr(3,:),'r','LineWidth',2)
+stairs(t,y,'k','LineWidth',2)
+stairs(t,ye,'r','LineWidth',2)
 % plot(t,u_t1,':k')
 H=legend('$r$','$y$', '$y_{estimado}$', 'Location', 'southeast', 'Orientation', 'vertical');
 set(H, 'Interpreter', 'latex', 'Fontsize', 22);
 ylabel('Altura (cm)', 'fontsize', 22)
 xlabel('Tempo (s)', 'fontsize', 22);
-T=title('Simulação do estimador do modelo ARX2');
-T=title('Resposta ao degrau do modelo SUB1 com estimador');
+T=title('Simulação do estimador do modelo ARXsim');
+T=title('Resposta ao degrau do modelo ARXsim com estimador');
 set(T, 'Interpreter', 'latex', 'Fontsize', 22);
 set(gca, 'fontsize', 20, 'Position',[0.155    0.16    0.8    0.75]);
-axis([0 simout.Time(end) min(min(yr))-.05 max(max(yr))+.4]);
+axis([0 10 min(min(y))-.05 max(max(y))+.4]);
--- Matlab Arquivos/grafico_step_simul.m
+++ Matlab Arquivos/grafico_step_simul.m
@@ -3,34 +3,35 @@ if max(y)<20
     p2=-3;
     y2=y;
     y=(y-p2)/p1;
-    ye=0-ye;
+    ye=ye;
     ye=(ye-p2)/p1;
     ref2=ref;
     ref=(ref-p2)/p1;
 end
 close all
+t=0:0.05:(length(y)-1)*0.05;
 figure('position', [50, 50, 1200, 500],'Color',[1 1 1])
-plot(ye,'LineWidth',1.5)
+stairs(t,ye,'LineWidth',1.5)
 hold on
-plot(ref,'r','LineWidth',1.5)
-plot(y,'k','LineWidth',1.5)
+stairs(t,ref,'r','LineWidth',1.5)
+stairs(t,y,'k','LineWidth',1.5)
 H=legend('$y_e$','$r$', '$y$', 'Location', 'northwest', 'Orientation', 'vertical');
 set(H, 'Interpreter', 'latex', 'Fontsize', 22);
 ylabel('Altura (cm)', 'fontsize', 22)
 xlabel('Tempo (s)', 'fontsize', 22);
-T=title('Resposta ao degrau do modelo ARXsim simulado');
+T=title('Resposta à escadaria do modelo ARXsim simulado');
 set(gca, 'fontsize', 20, 'Position',[0.155    0.16    0.8    0.75]);
 axis([0 40 min(min(ye))-.1 max(max(y))+.1]);
 
 figure('position', [50, 50, 1200, 500],'Color',[1 1 1])
-plot(y2-ref2,'k','LineWidth',1.5)
+stairs(t,y2-ref2,'k','LineWidth',1.5)
 hold on
-plot(u,'LineWidth',1.5)
+stairs(t,u,'LineWidth',1.5)
 H=legend('$e$','$u$', '$r$', 'Location', 'southeast', 'Orientation', 'vertical');
 set(H, 'Interpreter', 'latex', 'Fontsize', 22);
 % ylabel('Altura (cm)', 'fontsize', 22)
 xlabel('Tempo (s)', 'fontsize', 22);
-T=title('Resposta ao degrau do modelo ARXsim simulado');
+T=title('Resposta à escadaria do modelo ARXsim simulado');
 set(gca, 'fontsize', 20, 'Position',[0.155    0.16    0.8    0.75]);
 axis([0 40 min(min(y2-ref2))-.1 max(max(u))+.1]);
 
